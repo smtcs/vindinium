13a8128738254c490f45ff860e79ad478c6e89b7
ZucchiniZe <me@alexb.ninja>
2015-01-28 09:46:16 -0800

    register partials

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var routes = require('./routes/index');
 var bots = require('./routes/bots');
 
 var app = express();
+
+// register partials for handlebars
+hbs.registerPartials(__dirname + '/views/partials');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
