6e994acfc09aaa3456a11a4e7a4a854686b8718e
Matt Hesby <mhesby@summitps.org>
2015-02-13 01:40:58 -0500

    Trying to get the bot object to update when the different methods are called for it

--- test.js
+++ test.js
@@ -1,9 +1,28 @@
 var Bot = require('bot');
 
 var bot = new Bot('TEST KEY', 'training');
-
+console.log(bot);
 // bot test code
+function botInitialization(callback){
+  console.log("going to run bot! " + bot.mode);
+    bot.run(function(){
+      console.log("going to run afterPostRequest! " + bot.data);
+      bot.afterPostRequest(function(){
+        console.log("going to run botBrain! " + bot.data);
+        bot.botBrain(callback);
+      });
+    });
 
+}
+
+function botBrain(){
+  return 'north';
+}
+
+botInitialization(function(){console.log("finishedCycle!")});
+
+/*
 bot.run(function(data) {
   return 'north';
 });
+*/
