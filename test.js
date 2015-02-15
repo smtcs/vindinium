b12888ee74379e2d1bbd049b872d349e8c4802b4
Matt Hesby <mhesby@summitps.org>
2015-02-14 21:28:06 -0500

    tried async, is not waiting for post request to finish

--- test.js
+++ test.js
@@ -1,14 +1,60 @@
 var Bot = require('bot');
-
 var bot = new Bot('nwiltp4r', 'training');
+var gameStart = false;
+var _this = bot;
+var async = require('async');
+var goDir;
+var request = require('request');
+// bot.startGame    bot.parseTheData    bot.newRequest
+
+async.series([
+  function(callback){
+    console.log("async startGame()");
+    if(gameStart === false){
+      gameStart = true;
+      bot.startGame();
+
+    }
+    else{
+      bot.newRequest(goDir);
+    }
+    callback();
+  },
+  function(callback){
+    console.log("async parseTheData");
+    bot.parseTheData();
+    callback();
+  },
+  function(callback){
+    console.log("async botBrai");
+    botBrain();
+    callback();
+  }
+]);
+
+function botBrain(){
+  //console.log("botBrain!");
+  var rand = Math.floor(Math.random() * 3);
+  var dirs = ["north", "south", "east", "west"];
+  goDir = dirs[rand];
+
+}
+  
+
+
+
+
+
+////VERSION THAT CALLS ITSELF TOO MANY TIMES/////
+/*
 var goDir;
 //console.log(bot);
 // bot test code
 function botStart(callback){
  // console.log("going to run bot!");
-    bot.run(function(){
+    bot.startGame(function(){
      // console.log("going to run afterPostRequest!");
-      bot.afterPostRequest(function(){
+      bot.parseTheData(function(){
       //  console.log("going to run botBrain!");
         botBrain(function(){
           bot.requestAgain(goDir, botContinue());      
@@ -19,7 +65,7 @@ function botStart(callback){
 
 function botContinue(callback){
   //console.log("bot Continiue!");
-  bot.afterPostRequest(function(){
+  bot.parseTheData(function(){
     botBrain(function(){
       bot.requestAagain(goDir, botContinue());
     });
@@ -35,15 +81,14 @@ function botBrain(cb){
 
   //console.log(goDir);
 
-
-
-
-
   cb();
 }
 
 botStart();
+*/
 
+
+///// OLD VERSION /////
 /*
 bot.run(function(data) {
   return 'north';
