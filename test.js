8730b04a023ab722533bd50ec0f808c4382b4639
Matt Hesby <mhesby@summitps.org>
2015-02-18 03:09:13 -0500

    Switched pathfinder libraries and got it to work.  Added distance finder.  Added arrays of x,y location of different things on the map.  Cleaned up test.js to be default.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var Bot = require('bot');
+var PF = require('pathfinding');
 var bot = new Bot('nwiltp4r', 'training');
 var goDir;
 var Promise = require('bluebird');
@@ -9,26 +10,13 @@ Bot.prototype.botBrain = function(){
 /////////////Write your Bot Below Here//////////////////////
 ////////////Set bot.goDir in the direction you want to go///
 
-
-
   
     var rand = Math.floor(Math.random() * 3);
     var dirs = ["north", "south", "east", "west"];
-//    bot.helpers.pathfinder([3,4],[4,4]);
-//    console.log("pathtogo!: " + bot.pathDir);
-    console.log(bot.map);
-    console.log("bot id: " + bot.yourBot.id);
-    console.log("yourbot Pos : " + bot.yourBot.pos.x, bot.yourBot.pos.y);
-    console.log("bot 3 pos : " + bot.bot3.pos.x, bot.bot3.pos.y);
-    console.log("pathtogo!:  " + bot.pathDir );
-   // console.log("tarven position : " + _this.taverns[0]);
-
-    bot.goDir = bot.pathfinder([bot.yourBot.pos.x, bot.yourBot.pos.y], [8,6]);
-    console.log("going this direction!" + bot.goDir);
+    bot.goDir = dirs[rand];
 
 
 ///////////Do not remove anything below here////////////////
-   
     resolve();
   });
 }
